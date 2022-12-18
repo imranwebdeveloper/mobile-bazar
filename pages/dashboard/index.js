7f245dd319f79a12d2122ccdb50b9221a4ff00bc
Imran Nazir <imran.web.developer2021@gmail.com>
2022-12-18 08:58:55 +0600

    added dislimer

--- pages/dashboard/index.js
+++ pages/dashboard/index.js
@@ -3,16 +3,7 @@ import Dashboard from "../../components/Wrapper/Dashboard";
 const Home = () => {
   return (
     <Dashboard>
-      <h1>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae
-        repellendus reprehenderit est molestias quod, a omnis dicta veritatis
-        aspernatur error, eveniet temporibus! Delectus quisquam labore accusamus
-        reprehenderit deleniti, omnis ad. Lorem, ipsum dolor sit amet
-        consectetur adipisicing elit. Officiis quia reiciendis laborum autem
-        aspernatur, consequuntur obcaecati optio eos cumque atque laboriosam
-        nesciunt sit expedita accusantium! Nostrum, voluptatibus odio. Sequi,
-        necessitatibus?
-      </h1>
+      <h1 className="text-3xl">Page Under Working ...</h1>
     </Dashboard>
   );
 };
