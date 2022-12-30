dc1e33c999a0cbf7e2dc2b8c8018e0acd41863df
Imran Nazir <imran.web.developer2021@gmail.com>
2022-12-30 16:05:40 +0600

    used button instead of Link in login page

--- components/Common/button/FacebookBtn.js
+++ components/Common/button/FacebookBtn.js
@@ -4,8 +4,7 @@ import Link from "next/link";
 
 const FacebookBtn = () => {
   return (
-    <Link
-      href="/api/auth/facebook"
+    <button
       onClick={(e) => {
         e.preventDefault();
         signIn("facebook", { callbackUrl: "/" });
@@ -22,7 +21,7 @@ const FacebookBtn = () => {
           d="M279.14 288l14.22-92.66h-88.91v-60.13c0-25.35 12.42-50.06 52.24-50.06h40.42V6.26S260.43 0 225.36 0c-73.22 0-121.08 44.38-121.08 124.72v70.62H22.89V288h81.39v224h100.17V288z"
         />
       </svg>
-    </Link>
+    </button>
   );
 };
 
--- components/Common/button/GoogleBtn.js
+++ components/Common/button/GoogleBtn.js
@@ -3,8 +3,7 @@ import Link from "next/link";
 
 const GoogleBtn = () => {
   return (
-    <Link
-      href="/api/auth/google"
+    <button
       className="inline-block px-6 py-2.5  bg-[#ea4335] text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:shadow-lg focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg transition duration-150 ease-in-out"
       onClick={(e) => {
         e.preventDefault();
@@ -21,7 +20,7 @@ const GoogleBtn = () => {
           d="M488 261.8C488 403.3 391.1 504 248 504 110.8 504 0 393.2 0 256S110.8 8 248 8c66.8 0 123 24.5 166.3 64.9l-67.5 64.9C258.5 52.6 94.3 116.6 94.3 256c0 86.5 69.1 156.6 153.7 156.6 98.2 0 135-70.4 140.8-106.9H248v-85.3h236.1c2.3 12.7 3.9 24.9 3.9 41.4z"
         />
       </svg>
-    </Link>
+    </button>
   );
 };
 
