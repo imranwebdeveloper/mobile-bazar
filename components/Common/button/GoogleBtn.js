f47cf8fb945433774fc968f0e17ac839780e13ed
Imran Nazir <imran.web.developer2021@gmail.com>
2022-12-30 16:23:08 +0600

    added link instead of button

--- components/Common/button/GoogleBtn.js
+++ components/Common/button/GoogleBtn.js
@@ -3,7 +3,8 @@ import Link from "next/link";
 
 const GoogleBtn = () => {
   return (
-    <button
+    <Link
+      href="/api/auth/google"
       className="inline-block px-6 py-2.5  bg-[#ea4335] text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:shadow-lg focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg transition duration-150 ease-in-out"
       onClick={(e) => {
         e.preventDefault();
@@ -20,7 +21,7 @@ const GoogleBtn = () => {
           d="M488 261.8C488 403.3 391.1 504 248 504 110.8 504 0 393.2 0 256S110.8 8 248 8c66.8 0 123 24.5 166.3 64.9l-67.5 64.9C258.5 52.6 94.3 116.6 94.3 256c0 86.5 69.1 156.6 153.7 156.6 98.2 0 135-70.4 140.8-106.9H248v-85.3h236.1c2.3 12.7 3.9 24.9 3.9 41.4z"
         />
       </svg>
-    </button>
+    </Link>
   );
 };
 
